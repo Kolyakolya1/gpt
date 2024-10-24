8c56d7627b61c2e21b323dfef3ea0de37e915714
Mykola Adamchuk <dev9@eliteexperience.at>
2024-10-24 18:51:02 +0300

    start

--- resources/views/crm/openai/parse-voice.blade.php
+++ resources/views/crm/openai/parse-voice.blade.php
@@ -61,14 +61,8 @@
                 };
 
                 recognition.onresult = function(event) {
-                    const transcript = event.results[event.resultIndex][0].transcript.trim(); // Удаляем лишние пробелы
+                    const transcript = event.results[event.resultIndex][0].transcript;
                     console.log('Распознанный текст: ', transcript);
-
-                    // Проверка длины распознанного текста
-                    if (transcript.length < 3) {
-                        console.log('Распознанный текст слишком короткий, запрос не будет отправлен.');
-                        return; // Не отправляем запрос
-                    }
 
                     // Добавляем текст в чат
                     appendUserMessage(transcript);
@@ -88,7 +82,6 @@
                     // Отправляем распознанный текст на сервер
                     sendTranscriptionToServer(transcript, currentButton);
                 };
-
 
                 recognition.onerror = function(event) {
                     console.error('Ошибка распознавания: ', event.error);
