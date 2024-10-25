bf5e5c99dfa00eae77172641bc8e88004f2e658e
Mykola Adamchuk <dev9@eliteexperience.at>
2024-10-25 18:11:46 +0300

    start

--- routes/ai.php
+++ routes/ai.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Ai\AiController;
 
 Route::post('/send', [AiController::class, 'sendAudio'])->name('send-audio');
-Route::get('/voice', [AiController::class, 'parseVoice'])->name('parse.voice');
+Route::get('/voice1', [AiController::class, 'parseVoice'])->name('parse.voice');
